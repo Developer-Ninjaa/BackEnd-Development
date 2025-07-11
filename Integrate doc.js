256a93386aa5606fc29c3bc3e50861f4b145a329
Jaskaran Singh <89383365+Developer-Ninjaa@users.noreply.github.com>
2025-07-11 23:00:09 +0530

    Create Integrate doc.js

---
+++ Integrate doc.js
@@ -0,0 +1,6 @@
+integrate APIs to backend.
+generate access tokens through API.
+bearer token, fill the values create collections on postman
+provide authorisation - bearer token
+body-JSON {} 
+headers - accept - application/json
